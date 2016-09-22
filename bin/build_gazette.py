193e397877a8d0ff0d3f9c72871bc5769836ec44
krikit <krikit@naver.com>
2016-09-22 18:14:30 +0900

    #16 replace Counter to defaultdict(int)

--- bin/build_gazette.py
+++ bin/build_gazette.py
@@ -16,7 +16,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 import codecs
-from collections import Counter, defaultdict
+from collections import defaultdict
 import json
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -36,7 +36,7 @@ def _proc_gazette(dic):
     :param  dic:  dictionary
     :return:      category counter
     """
-    category_cnt = Counter()
+    category_cnt = defaultdict(int)
     for line in codecs.open(_OPTS.gazette, 'rt', encoding='UTF-8'):
         line = line.rstrip('\r\n')
         if not line:
@@ -58,7 +58,7 @@ def _proc_corpus(dic):
     :param  dic:  dictionary
     :return:      category counter
     """
-    category_cnt = Counter()
+    category_cnt = defaultdict(int)
     json_obj = json.load(codecs.open(_OPTS.corpus, 'rt', encoding='UTF-8'))
     for sent in json_obj['sentence']:
         for entity in sent['NE']:
@@ -80,16 +80,20 @@ def main():
     """
     build integrated gazette dictionary
     """
-    dic = defaultdict(Counter)
+    dic = defaultdict(lambda: defaultdict(int))
     total_cat_cnt = _proc_gazette(dic)
-    total_cat_cnt.update(_proc_corpus(dic))
+    # total_cat_cnt.update(_proc_corpus(dic))    # Counter only in 2.7
+    for key, val in _proc_corpus(dic).items():
+        total_cat_cnt[key] += val
 
     for key, cnt in dic.items():
         if re.match(r'^0+$', key) and set(cnt.keys()) <= set(['DT', 'TI']):
             # filter out only numbers of DT/TI
             continue
-        freq_cat_pairs = [(freq + total_cat_cnt[cat] / 1000000.0, cat)
-                          for cat, freq in cnt.most_common()]
+        # freq_cat_pairs = [(freq + total_cat_cnt[cat] / 1000000.0, cat)
+        #                   for cat, freq in cnt.most_common()]    # Counter only in 2.7
+        cnt_srt = sorted(cnt.items(), key=lambda itm: itm[1], reverse=True)
+        freq_cat_pairs = [(freq + total_cat_cnt[cat] / 1000000.0, cat) for cat, freq in cnt_srt]
         categories = [cat for freq, cat in sorted(freq_cat_pairs, reverse=True)]
         print('%s\t%s' % (key, ','.join(categories)))
 
